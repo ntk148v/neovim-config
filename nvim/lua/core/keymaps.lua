00ead7a710296e930bf8ee4753c689df10dd85c6
Kien Nguyen <kiennt2609@gmail.com>
2022-11-24 15:51:03 +0700

    Update keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -14,10 +14,10 @@ vim.keymap.set('n', '<leader>q', ':qa!<CR>', {})
 -- Fast saving with <leader> and s
 vim.keymap.set('n', '<leader>s', ':w<CR>', {})
 -- Move around splits using Ctrl + {h,j,k,l}
-vim.keymap.set('n', '<leader>h', '<C-w>h', {})
-vim.keymap.set('n', '<leader>j', '<C-w>j', {})
-vim.keymap.set('n', '<leader>k', '<C-w>k', {})
-vim.keymap.set('n', '<leader>l', '<C-w>l', {})
+vim.keymap.set('n', '<leader>wh', '<C-w>h', {})
+vim.keymap.set('n', '<leader>wj', '<C-w>j', {})
+vim.keymap.set('n', '<leader>wk', '<C-w>k', {})
+vim.keymap.set('n', '<leader>wl', '<C-w>l', {})
 
 -- Reload configuration without restart nvim
 vim.keymap.set('n', '<leader>r', ':so %<CR>', {})

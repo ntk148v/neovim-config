c4e5b40cdaaaf6a96b4d3a89a01663be667f23b6
Kien Nguyen <kiennt2609@gmail.com>
2022-12-20 15:35:10 +0700

    Disable statusline in file tree and packer

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -101,6 +101,7 @@ local config = {
         -- Disable sections and component separators
         component_separators = '',
         section_separators = '',
+        disabled_filetypes = {'packer', 'NvimTree'},
         theme = {
             -- We are going to use lualine_c an lualine_x as left and
             -- right section. Both are highlighted by c theme .  So we

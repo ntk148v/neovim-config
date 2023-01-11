7174ed94d264d60409b8d73a58969e73c4f84fc1
Kien Nguyen <kiennt2609@gmail.com>
2023-01-11 15:48:40 +0700

    Wrap text if file type is markdown

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -17,11 +17,7 @@ local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 -- General settings
 
 -- Highlight on yank
-augroup('YankHighlight', {
-    clear = true
-})
 autocmd('TextYankPost', {
-    group = 'YankHighlight',
     callback = function()
         vim.highlight.on_yank({
             higroup = 'IncSearch',
@@ -49,23 +45,21 @@ autocmd('BufEnter', {
     command = 'set fo-=c fo-=r fo-=o'
 })
 
--- Set indentation to 2 spaces
-augroup('setIndent', {
-    clear = true
-})
 autocmd('Filetype', {
-    group = 'setIndent',
     pattern = {'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript', 'yaml', 'lua'},
     command = 'setlocal shiftwidth=2 tabstop=2'
 })
 
 -- Set colorcolumn
-augroup('setColorColumn', {
-    clear = true
+autocmd('Filetype', {
+    pattern = {'python', 'rst', 'c', 'cpp'},
+    command = 'set colorcolumn=80'
 })
 
 autocmd('Filetype', {
-    group = 'setColorColumn',
-    pattern = {'python', 'rst', 'c', 'cpp'},
-    command = 'set colorcolumn=80 nowrap'
+    pattern = {"gitcommit", "markdown", "text"},
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end
 })
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -67,7 +67,7 @@ opt.sidescrolloff = 3 -- minimal number of screen columns to keep to the left an
 opt.signcolumn = 'yes' -- always show the sign column, otherwise it would shift the text each time
 opt.splitbelow = true -- open new split below
 opt.splitright = true -- open new split to the right
-opt.wrap = true -- display long line as multiple lines
+opt.wrap = false -- display a long line
 
 -- backups
 opt.backup = false -- create a backup file

606032a045bda410a76022979554786327a309ac
Kien Nguyen <kiennt2609@gmail.com>
2023-01-13 10:08:54 +0700

    Change character

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -49,8 +49,9 @@ opt.cursorline = true -- highlight the current line
 opt.laststatus = 2 -- only the last window will always have a status line
 opt.lazyredraw = true -- don't update the display while executing macros
 opt.list = true
+-- You can also add 'space' or 'eol', but I feel it's quite annoying
 opt.listchars = {
-    tab = '| ',
+    tab = '┊ ',
     trail = '·',
     extends = '»',
     precedes = '«',

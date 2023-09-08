cd79516481a9ceabeb240a65b2db6dce6ad04a14
Kien Nguyen <kiennt2609@gmail.com>
2023-09-08 14:38:49 +0700

    fix: remove deprecated options

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -19,38 +19,7 @@ return {{
 }, {
     -- Github - Github"s Neovim themes
     "projekt0n/github-nvim-theme",
-    lazy = true,
-    opts = {
-        theme_style = "dark",
-        function_style = "italic",
-        sidebars = {"qf", "vista_kind", "terminal", "packer"},
-
-        -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-        colors = {
-            hint = "orange",
-            error = "#ff0000"
-        },
-
-        -- Overwrite the highlight groups
-        overrides = function(c)
-            return {
-                htmlTag = {
-                    fg = c.red,
-                    bg = "#282c34",
-                    sp = c.hint,
-                    style = "underline"
-                },
-                DiagnosticHint = {
-                    link = "LspDiagnosticsDefaultHint"
-                },
-                -- this will remove the highlight groups
-                TSField = {}
-            }
-        end
-    },
-    config = function(_, opts)
-        require("github-theme").setup(opts)
-    end
+    lazy = true
 }, {
     -- Tokyonight- A clean, dark Neovim theme written in Lua, with support for lsp,
     -- treesitter and lots of plugins. Includes additional themes for Kitty, Alacritty, iTerm and Fish.

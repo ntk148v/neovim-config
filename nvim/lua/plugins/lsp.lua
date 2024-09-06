fd76db5a102ed54626de347f4081a0fd5323d637
Kien Nguyen <kiennt2609@gmail.com>
2024-09-06 10:14:58 +0700

    fix: replace deprecated lsp plugin
    jose-elias-alvarez/null-ls.nvim has been archived on Aug 12, 2023 and is no longer maintained.
    The use of this plugin causes a bunch of warnings in neovim because of some functions being
    deprecated that are used in the plugin.
    
    Switch to nvimtools/none-ls.nvim which is the successor of the previous plugin.
    It uses the same API, so it shouldn't affect the functionality in which null-ls.nvim
    was previously utilized.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -277,14 +277,13 @@ return {{
 }, {
     -- Use Neovim as a language server to inject LSP diagnostics,
     -- code actions, and more via Lua.
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {"williamboman/mason.nvim", "nvim-lua/plenary.nvim"},
+    "nvimtools/none-ls.nvim",
     lazy = false,
     config = function()
         local null_ls = require("null-ls")
-        -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+        -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
         local formatting = null_ls.builtins.formatting
-        -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+        -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
         local diagnostics = null_ls.builtins.diagnostics
 
         null_ls.setup({

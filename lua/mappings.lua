360d92d1dd94f6b096217aee61c091fb691b7ab1
Kien Nguyen <kiennt2609@gmail.com>
2025-11-08 10:58:33 +0700

    update options, mappings and more instructions for newbie

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,14 +14,11 @@
 local map = vim.keymap.set
 local cmd = vim.cmd
 
-map("n", "<leader>q", ":qa!<CR>", {})
--- Fast saving with <leader> and s
-map("n", "<leader>s", ":w<CR>", {})
 -- Move around splits
-map("n", "<leader>wh", "<C-w>h", { desc = "switch window left" })
-map("n", "<leader>wj", "<C-w>j", { desc = "switch window right" })
-map("n", "<leader>wk", "<C-w>k", { desc = "switch window up" })
-map("n", "<leader>wl", "<C-w>l", { desc = "switch window down" })
+map("n", "<leader>wh", "<C-w>h", { desc = "Switch [W]indow left" })
+map("n", "<leader>wj", "<C-w>j", { desc = "Switch [W]indow right" })
+map("n", "<leader>wk", "<C-w>k", { desc = "Switch [W]indow up" })
+map("n", "<leader>wl", "<C-w>l", { desc = "Switch [W]indow down" })
 
 -- Reload configuration without restart nvim
 -- Or you don't want to use plenary.nvim, you can use this code
@@ -43,42 +40,66 @@ function _G.reload_config()
     vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
 end
 
-map("n", "rr", _G.reload_config, { desc = "Reload configuration without restart nvim" })
+map("n", "rr", _G.reload_config, { desc = "[R]eload configuration without restart nvim" })
 
 -- Telescope
 local builtin = require "telescope.builtin"
-map("n", "<leader>ff", builtin.find_files, { desc = "Open Telescope to find files" })
-map("n", "<leader>fg", builtin.live_grep, { desc = "Open Telescope to do live grep" })
-map("n", "<leader>fb", builtin.buffers, { desc = "Open Telescope to list buffers" })
-map("n", "<leader>fh", builtin.help_tags, { desc = "Open Telescope to show help" })
-map("n", "<leader>fo", builtin.oldfiles, { desc = "Open Telescope to list recent files" })
-map("n", "<leader>cm", builtin.git_commits, { desc = "Open Telescope to list git commits" })
+map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+map('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+map("n", "<leader>sc", builtin.git_commits, { desc = "[S]earch git commits" })
+map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+map('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+-- Slightly advanced example of overriding default behavior and theme
+map('n', '<leader>/', function()
+    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+-- It's also possible to pass additional configuration options.
+--  See `:help telescope.builtin.live_grep()` for information about particular keys
+map('n', '<leader>s/', function()
+    builtin.live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+    }
+end, { desc = '[S]earch [/] in Open Files' })
+
+-- Shortcut for searching your Neovim configuration files
+map('n', '<leader>sn', function()
+    builtin.find_files { cwd = vim.fn.stdpath 'config' }
+end, { desc = '[S]earch [N]eovim files' })
 
 -- Mini.files
 map(
     "n",
     "<leader>fm",
     function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end,
-    { desc = "Open mini.files (Directory of Current File)"}
+    { desc = "Open [m]ini.files (Directory of Current File)" }
 )
 
 map(
     "n",
     "<leader>fM",
     function() require("mini.files").open(vim.uv.cwd(), true) end,
-    { desc = "Open mini.files (cwd)"}
+    { desc = "Open [M]ini.files (cwd)" }
 )
 
--- LSP
+-- Autoformat
 map(
     "n",
-    "<leader>gm",
+    "<leader>l",
     function() require("conform").format { lsp_fallback = true } end,
-    { desc = "General Format file" }
+    { desc = "[F]ormat buffer" }
 )
-
--- global lsp mappings
-map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic loclist" })
 
 -- Comment
 map("n", "mm", "gcc", { desc = "Toggle comment", remap = true })
@@ -89,3 +110,10 @@ map("n", "tt", function()
     local height = math.floor(vim.o.lines / 2)
     cmd("belowright split | resize " .. height .. " | terminal")
 end, { noremap = true, silent = true })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })

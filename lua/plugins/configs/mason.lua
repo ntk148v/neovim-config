0eec6cc3a03449691e6646944b167e103dc8c867
Kien Nguyen <kiennt2609@gmail.com>
2026-02-20 22:31:05 +0700

    fix(mason): remove unnecessary return wrapper around setup call

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -9,28 +9,26 @@
 -- File: plugins/configs/mason.lua
 -- Description: mason
 -- Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
-return {
-    require("mason").setup({
-        PATH = "prepend",
-        ui = {
-            icons = {
-                package_pending = " ",
-                package_installed = "󰄳 ",
-                package_uninstalled = "󰚌 ",
-            },
-
-            keymaps = {
-                toggle_server_expand = "<CR>",
-                install_server = "i",
-                update_server = "u",
-                check_server_version = "c",
-                update_all_servers = "U",
-                check_outdated_servers = "C",
-                uninstall_server = "X",
-                cancel_installation = "<C-c>",
-            },
+require("mason").setup({
+    PATH = "prepend",
+    ui = {
+        icons = {
+            package_pending = " ",
+            package_installed = "󰄳 ",
+            package_uninstalled = "󰚌 ",
         },
 
-        max_concurrent_installers = 10,
-    }),
-}
+        keymaps = {
+            toggle_server_expand = "<CR>",
+            install_server = "i",
+            update_server = "u",
+            check_server_version = "c",
+            update_all_servers = "U",
+            check_outdated_servers = "C",
+            uninstall_server = "X",
+            cancel_installation = "<C-c>",
+        },
+    },
+
+    max_concurrent_installers = 10,
+})

e5c3eb9ac7dd991e68af530211c47eb024660202
Kien Nguyen <kiennt2609@gmail.com>
2026-02-20 22:49:01 +0700

    refactor(lspconfig): remove unused opts and duplicate mason.setup
    - Remove unused autoformat and format options
    - Remove duplicate mason.setup() call (already in mason.lua)
    - Simplify setup function by removing unused setup hook

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -39,51 +39,14 @@ local formatting_servers = {
     },
 }
 
--- Merge
 merge_tables(formatting_servers, custom_formatting_servers)
 
-local opts = {
-    -- Automatically format on save
-    autoformat = true,
-    -- options for vim.lsp.buf.format
-    -- `bufnr` and `filter` is handled by the LazyVim formatter,
-    -- but can be also overridden when specified
-    format = {
-        formatting_options = nil,
-        timeout_ms = nil,
-    },
-    -- LSP Server Settings
-    servers = formatting_servers,
-    -- you can do any additional lsp server setup here
-    -- return true if you don"t want this server to be setup with lspconfig
-    setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-    },
-}
-
-local servers = opts.servers
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local function setup(server)
     local server_opts = vim.tbl_deep_extend("force", {
         capabilities = vim.deepcopy(capabilities),
-    }, servers[server] or {})
-
-    if opts.setup[server] then
-        if opts.setup[server](server, server_opts) then
-            return
-        end
-    elseif opts.setup["*"] then
-        if opts.setup["*"](server, server_opts) then
-            return
-        end
-    end
+    }, formatting_servers[server] or {})
     require("lspconfig")[server].setup(server_opts)
 end
 
@@ -101,11 +64,10 @@ do
     end
 end
 
-local ensure_installed = {} ---@type string[]
-for server, server_opts in pairs(servers) do
+local ensure_installed = {}
+for server, server_opts in pairs(formatting_servers) do
     if server_opts then
         server_opts = server_opts == true and {} or server_opts
-        -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
         if server_opts.mason == false or not vim.tbl_contains(available, server) then
             setup(server)
         else
@@ -114,10 +76,8 @@ for server, server_opts in pairs(servers) do
     end
 end
 
-require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = ensure_installed,
     automatic_installation = true,
-    -- Whether installed servers should automatically be enabled via `:h vim.lsp.enable()`.
     automatic_enable = true,
 })

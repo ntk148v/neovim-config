c3c256a750b183b4255ae4715aa4592fd6b522e5
Kien Nguyen Tuan <kiennt2609@gmail.com>
2026-03-20 15:32:11 +0700

    fix(lualine): migrate to nvim v0.11+
    Replace deprecated vim.lsp.get_active_clients() with vim.lsp.get_clients()
    for Neovim 0.11+ compatibility.

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -160,7 +160,7 @@ ins_right({
     function()
         local msg = "null"
         local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
             return msg
         end

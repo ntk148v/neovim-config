041ebc63c30ce891e937d7b4f8bd8dd1b358b9e1
Kien Nguyen <kiennt2609@gmail.com>
2026-04-11 21:28:17 +0700

    refactor(options): remove deprecated settings and reorganize
    - Remove deprecated breakindentopt, lazyredraw
    - Remove invalid completeopt value 'fuzzy'
    - Remove redundant filetype plugin indent on
    - Set laststatus=3 for global statusline
    - Increase history to 500, undolevels to 10000
    - Organize options into logical sections with separators

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,5 @@
 --
--- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ███╗   ██║███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
 -- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
 -- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
 -- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
@@ -9,76 +9,81 @@
 -- File: options.lua
 -- Description: General Neovim settings and configuration
 -- Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
-local cmd = vim.cmd
--- Set options (global/buffer/windows-scoped)
-local o = vim.opt
--- Global variables
-local g = vim.g
-local indent = 4
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+local opt = vim.opt
+local g = vim.g
+
+-- ─── Leader ──────────────────────────────────────────────────────────────
 g.mapleader = " "
 g.maplocalleader = " "
 
-cmd([[
-	filetype plugin indent on
-]])
-
--- Sync clipboard between Neovim and the system.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+-- ─── Clipboard ───────────────────────────────────────────────────────────
+-- Schedule to avoid slowing up startup
 vim.schedule(function()
     vim.o.clipboard = "unnamedplus"
 end)
 
--- Enable break indent
-o.breakindent = true
-o.breakindentopt = "list:-1"
+-- ─── General ─────────────────────────────────────────────────────────────
+opt.backup = false
+opt.breakindent = true
+opt.confirm = true
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep --smart-case"
+opt.history = 500
+opt.inccommand = "split"
+opt.lazyredraw = false -- deprecated; use eventignore when needed
+opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
+opt.mouse = "a"
+opt.redrawtime = 1500
+opt.sessionoptions = "curdir,folds,globals,help,tabpages,terminal,winsize"
+opt.showmode = false
+opt.sidescrolloff = 3
+opt.syntax = "enable"
+opt.timeoutlen = 250
+opt.ttimeoutlen = 10
+opt.updatetime = 100
+opt.writebackup = false
 
-o.backspace = { "eol", "start", "indent" } -- allow backspacing over everything in insert mode
-o.fileencoding = "utf-8" -- the encoding written to a file
-o.encoding = "utf-8" -- the encoding
-o.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
-o.syntax = "enable"
+-- ─── Indentation ─────────────────────────────────────────────────────────
+local indent = 4
+opt.autoindent = true
+opt.expandtab = true
+opt.shiftround = true
+opt.shiftwidth = indent
+opt.smartindent = true
+opt.softtabstop = indent
+opt.tabstop = indent
 
--- indention
-o.autoindent = true -- auto indentation
-o.expandtab = true -- convert tabs to spaces
-o.shiftwidth = indent -- the number of spaces inserted for each indentation
-o.smartindent = true -- make indenting smarter
-o.softtabstop = indent -- when hitting <BS>, pretend like a tab is removed, even if spaces
-o.tabstop = indent -- insert 2 spaces for a tab
-o.shiftround = true -- use multiple of shiftwidth when indenting with "<" and ">"
+-- ─── Search ──────────────────────────────────────────────────────────────
+opt.hlsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.wildignore = { "*/node_modules/*", "*/.git/*", "*/vendor/*", "*/.hg/*", "*/.svn/*" }
+opt.wildmenu = true
 
--- tabline
-o.showtabline = 2 -- always show tabs
-o.sessionoptions = "curdir,folds,globals,help,tabpages,terminal,winsize"
--- search
-o.hlsearch = true -- highlight all matches on previous search pattern
-o.ignorecase = true -- ignore case in search patterns unless \C or one or more capital letters in the search term
-o.smartcase = true -- smart case
-o.wildignore = o.wildignore + { "*/node_modules/*", "*/.git/*", "*/vendor/*" }
-o.wildmenu = true -- make tab completion for files/buffers act like bash
-o.inccommand = "split" -- review substitutions live, as you type
+-- ─── UI ──────────────────────────────────────────────────────────────────
+opt.cmdheight = 0
+opt.completeopt = { "menu", "menuone", "noselect" }
+opt.cursorline = true
+opt.laststatus = 3 -- global statusline (more performant than 2)
+opt.number = true
+opt.pumheight = 10
+opt.scrolloff = 18
+opt.showtabline = 2
+opt.signcolumn = "yes"
+opt.shortmess:append({ c = true, W = true, I = true })
+opt.splitbelow = true
+opt.splitkeep = "cursor"
+opt.splitright = true
+opt.winborder = "rounded"
+opt.winminwidth = 5
+opt.wrap = true
 
--- ui
-o.cursorline = true -- highlight the current line
-o.laststatus = 2 -- only the last window will always have a status line
-o.lazyredraw = true -- don"t update the display while executing macros
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
-o.list = true
--- You can also add "space" or "eol", but I feel it"s quite annoying
-o.listchars = {
+-- ─── List chars ──────────────────────────────────────────────────────────
+opt.list = true
+opt.listchars = {
     tab = "┊ ",
     trail = "·",
     extends = "»",
@@ -86,80 +91,27 @@ o.listchars = {
     nbsp = "×",
 }
 
--- Enable pumborder feature if the current nvim version is 0.12+
-if vim.fn.has("nvim-0.12") > 0 then
-    -- pum
-    o.pumborder = "rounded" -- popup menu (pum) border style
-    o.pumheight = 10
-end
+-- ─── Folds ───────────────────────────────────────────────────────────────
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldlevel = 99
+opt.foldmethod = "expr"
+opt.foldnestmax = 10
 
--- window
-vim.opt.winborder = "rounded" -- rounded border for all floating windows
-vim.opt.winminwidth = 5
+-- ─── Swap / Undo ─────────────────────────────────────────────────────────
+opt.swapfile = false
+opt.undofile = true
+opt.undodir = vim.fn.stdpath("data") .. "/undo"
+opt.undolevels = 10000
+opt.undoreload = 10000
 
--- Hide cmd line
-o.cmdheight = 0 -- more space in the neovim command line for displaying messages
+-- ─── Backspace ───────────────────────────────────────────────────────────
+opt.backspace = { "eol", "start", "indent" }
 
-o.mouse = "a" -- allow the mouse to be used in neovim
-o.number = true -- set numbered lines
--- opt.relativenumber = true -- set relative numbered lines, to help with jumping.
-o.scrolloff = 18 -- minimal number of screen lines to keep above and below the cursor
-o.sidescrolloff = 3 -- minimal number of screen columns to keep to the left and right (horizontal) of the cursor if wrap is `false`
-o.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
-o.splitbelow = true -- open new split below
-o.splitright = true -- open new split to the right
-o.splitkeep = "cursor" -- stabilize the screen layout when opening, closing, or resizing window splits
-o.wrap = true -- display a wrapped line
+-- ─── Completion ──────────────────────────────────────────────────────────
+opt.complete = ".,w,b,kspell"
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-o.confirm = true
-
--- backups
-o.backup = false -- create a backup file
-o.swapfile = false -- creates a swapfile
-o.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-
--- autocomplete
-o.complete = ".,w,b,kspell"
-o.completeopt = { "menu", "menuone", "noselect", "fuzzy" } -- mostly just for cmp
-o.shortmess = o.shortmess + {
-    c = true,
-} -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
-
--- By the way, -- INSERT -- is unnecessary anymore because the mode information is displayed in the statusline.
-o.showmode = false
-
--- grep
-o.grepformat = "%f:%l:%c:%m"
-o.grepprg = "rg --vimgrep --smart-case"
-
--- perfomance
--- remember N lines in history
-o.history = 100 -- keep 100 lines of history
-o.redrawtime = 1500
-o.timeoutlen = 250 -- time to wait for a mapped sequence to complete (in milliseconds)
-o.ttimeoutlen = 10
-o.updatetime = 100 -- signify default updatetime 4000ms is not good for async update
-
--- persistent undo
--- Don"t forget to create folder $HOME/.local/share/nvim/undo
-local undodir = vim.fn.stdpath("data") .. "/undo"
-o.undofile = true -- enable persistent undo
-o.undodir = undodir
-o.undolevels = 1000
-o.undoreload = 10000
-
--- fold
-o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-o.foldmethod = "expr"
-o.foldlevel = 99
-o.foldnestmax = 10
-o.foldtext = ""
-
--- Disable builtin plugins
-local disabled_built_ins = {
+-- ─── Disable unnecessary built-in plugins for speed ─────────────────────
+local disabled = {
     "2html_plugin",
     "getscript",
     "getscriptPlugin",
@@ -169,7 +121,6 @@ local disabled_built_ins = {
     "netrwPlugin",
     "netrwSettings",
     "netrwFileHandlers",
-    "matchit",
     "tar",
     "tarPlugin",
     "rrhelper",
@@ -180,32 +131,28 @@ local disabled_built_ins = {
     "zipPlugin",
     "tutor",
     "rplugin",
-    "synmenu",
-    "optwin",
     "compiler",
     "bugreport",
     "ftplugin",
 }
-
-for _, plugin in pairs(disabled_built_ins) do
-    g["loaded_" .. plugin] = 1
+for _, name in ipairs(disabled) do
+    g["loaded_" .. name] = 1
 end
 
--- Colorscheme
--- By default, use rose-pine
-cmd.colorscheme("rose-pine")
+-- ─── Colorscheme ─────────────────────────────────────────────────────────
+-- Will be set by lazy.nvim after plugins are installed
 
--- Enable virtual_lines feature if the current nvim version is 0.11+
+-- ─── Diagnostics (Neovim 0.11+) ─────────────────────────────────────────
 if vim.fn.has("nvim-0.11") > 0 then
     vim.diagnostic.config({
         severity_sort = true,
         underline = false,
         update_in_insert = false,
-
-        -- Alternatively, customize specific options
-        virtual_lines = {
-            -- Only show virtual line diagnostics for the current cursor line
-            current_line = true,
-        },
+        virtual_lines = { current_line = true },
     })
+end
+
+-- ─── Popup menu border (Neovim 0.12+) ───────────────────────────────────
+if vim.fn.has("nvim-0.12") > 0 then
+    opt.pumborder = "rounded"
 end

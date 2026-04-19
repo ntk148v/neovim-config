29d0765a18b43ec008a5ae722f77b244c79164c5
Kien Nguyen <kiennt2609@gmail.com>
2026-04-19 10:48:35 +0700

    fix(nvim-autopair): register the handler as a call
    The issue was caused by immediately invoking the on_confirm_done
    handler with the cmp module as an argument, which triggered a
    "nil value" error.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -303,6 +303,9 @@ local cmp = {
             },
             config = function(_, opts)
                 require("nvim-autopairs").setup(opts)
+                local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+                local cmp = require("cmp")
+                cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
             end,
         },
 
